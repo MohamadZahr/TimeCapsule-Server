0f1d78f7c14cb6451fb65a48e66a10f25938b92e
MohamadZahr <mohamadzahr04@gmail.com>
2025-07-16 23:28:15 +0300

    migrations done

---
+++ database/migrations/2025_07_16_201307_create_capsules_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('capsules', function (Blueprint $table) {
+            $table->id();
+            $table->integer ('user_id')->references('id')->on('users')->onDelete('cascade')  ;
+            $table->string('title');
+            $table->text('message');
+            $table->boolean('private')->default(0);
+            $table->boolean('surprise')->default(0);
+            $table->string('image_path')->nullable();
+            $table->string('audio_path')->nullable();
+            $table->dateTime('revealed_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('capsules');
+    }
+};
--- database/migrations/2025_07_16_202023_create_exports_table.php
+++ database/migrations/2025_07_16_202023_create_exports_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('exports', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->integer('capsule_id')->references('id')->on('capsules')->onDelete('cascade');
+            $table->string('file_path')->nullable();
             $table->timestamps();
-        });;
+        });
     }
 
     /**
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('exports');
     }
 };
