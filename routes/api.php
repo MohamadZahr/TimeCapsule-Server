c4272defceb3434c27df7aa032ca2cbf4cdf39b7
MohamadZahr <mohamadzahr04@gmail.com>
2025-07-19 15:38:24 +0300

    Auth complete

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\Common\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(["prefix" => "v0.1"], function () {
+    Route::group(["middleware" => "auth:api"], function () {
+
+        //AUTHENTICATED APIs
+
+        // Route::get("/tasks", [TaskController::class, "getAllTasks"]);
+        // Route::post("/add_update_task/{id?}", [TaskController::class, "addOrUpdateTask"]);
+    });
+
+    //UNAUTHENTICATED APIs
+    Route::group(["prefix" => "guest"], function () {
+        Route::post("/login", [AuthController::class, "login"]);
+        Route::post("/register", [AuthController::class, "register"]);
+    });
+});
